b44ddcf47879d0efc0533d2b0a95651f949e0f83
emanuelecoppo <emanuelecoppo@gmail.com>
2019-02-05 23:06:15 +0100

    Primo

--- variable.js
+++ variable.js
@@ -1,1 +1,17 @@
+$('.font').attr({
+  contenteditable: true,
+  spellcheck: false
+});
 
+$('#Muybridge').attr({
+  contenteditable: false,
+});
+
+$('.font').each(function() {
+  $(this).css( 'font-family', $(this).attr('id') );
+  $(this).css( 'animation-name', $(this).attr('id') );
+});
+
+$('#pause').click(function() {
+    $('.font').toggleClass('running paused');
+});
